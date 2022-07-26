197d67ed9b75f747d618f426366f5321f93d0dd7
DakBuk <brucdarc@gmail.com>
2022-07-26 14:38:08 -0600

    clean test file a little

--- test/Minter.js
+++ test/Minter.js
@@ -14,21 +14,11 @@ describe("Deploy Clones", function () {
 
     await Factory.deployed()
 
-    console.log(Factory.address);
-
     CollectionNft = await ethers.getContractFactory("MinterNftV2");
 
     const DefaultBase = await CollectionNft.deploy();
 
     await Factory.updateBase(DefaultBase.address);
-
-    const Hmm = await DefaultBase.name();
-
-    console.log(Hmm, "name");
-
-    const nextIdBase = await DefaultBase.nextId();
-
-    console.log(nextIdBase, "NIDOG")
 
   })
 
@@ -36,11 +26,7 @@ describe("Deploy Clones", function () {
 
     let addr1 = await Factory.predictAddress(0);
 
-    console.log(addr1, "addr1");
-
-    await Factory.deployCollection("Gr8 nft","BONG", owner.address);
-
-    let addr2 = await Factory.predictAddress(1);
+    await Factory.deployCollection("Best NFTs", "BNFT", owner.address);
 
     const justDeployed = await CollectionNft.attach(
         addr1
@@ -48,15 +34,11 @@ describe("Deploy Clones", function () {
 
     const nextId = await justDeployed.nextId();
 
-    console.log(nextId, "NID")
-
     const name = await justDeployed.name();
 
-    console.log(name, "name new");
-
-    console.log(addr2, "addr2")
-
     await Factory.deployCollection("Gr8 nft","BONG", owner.address);
+
+    let addr2 = await Factory.predictAddress(1);
 
     const justDeployedOld = await CollectionNft.attach(
         addr2
@@ -64,24 +46,14 @@ describe("Deploy Clones", function () {
 
     const nameOld = await justDeployedOld.name();
 
-    console.log(nameOld, "name old");
-
     const nextIdOld = await justDeployedOld.nextId();
-
-    console.log(nextIdOld, "NIDO")
-
-    console.log(owner.address, "oner")
 
     await justDeployed.mintCallback(owner.address, 1234);
 
     const tokenId = await justDeployed.mintIdToTokenId(1234);
 
-    console.log(tokenId, "TID")
-
     await justDeployed["safeTransferFrom(address,address,uint256)"](owner.address, "0x4e2d97538aa64b44326cf2e9065b65C3805863F3", 1)
 
 
-
-    //console.log("Here")
   })
 });
