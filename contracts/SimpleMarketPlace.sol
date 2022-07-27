d2e4b16109a90b911aa5768920615395d2afe782
DakBuk <brucdarc@gmail.com>
2022-07-27 17:27:20 -0600

    Marketplace function stubs

--- contracts/SimpleMarketPlace.sol
+++ contracts/SimpleMarketPlace.sol
@@ -5,6 +5,43 @@ contract SimpleMarketPlace{
 
 
 
+    function listNFT(
+        address _collection,
+        uint256 _id,
+        uint256 _onePrice,
+        address _listFor
+    )
+        external
+    {
 
+    }
+
+    function delistNFT(
+        address _collection,
+        uint256 _id
+    )
+        external
+    {
+
+    }
+
+    function changePrice(
+        address _collection,
+        uint256 _id,
+        uint256 _newPrice
+    )
+        external
+    {
+
+    }
+
+    function buyNFT(
+        address _collection,
+        uint256 _id
+    )
+        external
+    {
+
+    }
 
 }
