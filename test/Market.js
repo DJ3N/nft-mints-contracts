1ffad2b3420a70473a2589842c51ef0789b8eaf3
DakBuk <brucdarc@gmail.com>
2022-08-26 15:19:55 -0600

    Add oracle USD buys testing

--- test/Market.js
+++ test/Market.js
@@ -198,6 +198,101 @@ describe("Market", function () {
   });
 
   it('Nft sell in USD using chainlink price feed', async function () {
+    let addr1 = await Factory.predictAddress(0);
 
+    await Factory.deployCollection("Best NFTs", "BNFT", owner.address, 10);
+
+    const justDeployed = await CollectionNft.attach(
+        addr1
+    );
+
+    await justDeployed.bulkMintURI([owner.address, owner.address], ["We like beans", "beans are good"]);
+
+    await justDeployed.setApprovalForAll(MarketPlace.address, true)
+
+    await MarketPlace.setFee(ethers.utils.parseEther("1.05"))
+
+    await FakeChainLink.setCurrentPrice("200000000")
+
+    await MarketPlace.listNFT(
+        justDeployed.address,
+        1,
+        ethers.utils.parseEther("5"),
+        owner.address,
+        true
+    );
+
+    await MarketPlace.listNFT(
+        justDeployed.address,
+        2,
+        ethers.utils.parseEther("5"),
+        owner.address,
+        true
+    );
+
+    await expect(
+        MarketPlace.connect(bob).buyNFT(
+            justDeployed.address,
+            1,
+            {value: ethers.utils.parseEther("2.5")}
+        )
+    ).to.be.revertedWith("WRONG BUYOUT AMOUNT")
+
+    await expect(() =>
+        MarketPlace.connect(bob).buyNFT(
+            justDeployed.address,
+            1,
+            {value: ethers.utils.parseEther("2.625")}
+        )
+    ).to.changeEtherBalances(
+        [bob, owner],
+        [ethers.utils.parseEther("-2.625"), ethers.utils.parseEther("2.5")]
+    );
+
+    let ownerAfter = await justDeployed.ownerOf(1);
+
+    await expect(() =>
+        MarketPlace.collectFees()
+    ).to.changeEtherBalance(owner, ethers.utils.parseEther("0.125"));
+
+    await expect(
+        MarketPlace.connect(bob).collectFees()
+    ).to.be.revertedWith("Ownable: caller is not the owner")
+
+    expect(ownerAfter).to.equal(bob.address)
+
+    await FakeChainLink.setCurrentPrice("2500000000")
+    await FakeChainLink.setDecimals("10")
+
+    await expect(
+        MarketPlace.connect(bob).buyNFT(
+            justDeployed.address,
+            2,
+            {value: ethers.utils.parseEther("20")}
+        )
+    ).to.be.revertedWith("WRONG BUYOUT AMOUNT")
+
+    await expect(() =>
+        MarketPlace.connect(bob).buyNFT(
+            justDeployed.address,
+            2,
+            {value: ethers.utils.parseEther("250")}
+        )
+    ).to.changeEtherBalances(
+        [bob, owner],
+        [ethers.utils.parseEther("-21"), ethers.utils.parseEther("20")]
+    );
+
+    ownerAfter = await justDeployed.ownerOf(2);
+
+    await expect(() =>
+        MarketPlace.collectFees()
+    ).to.changeEtherBalance(owner, ethers.utils.parseEther("1"));
+
+    await expect(
+        MarketPlace.connect(bob).collectFees()
+    ).to.be.revertedWith("Ownable: caller is not the owner")
+
+    expect(ownerAfter).to.equal(bob.address)
   })
 });
