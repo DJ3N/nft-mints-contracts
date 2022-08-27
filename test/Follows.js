a9cf1b2e184c89a901da832a890823d09d6d1742
DakBuk <brucdarc@gmail.com>
2022-08-26 19:59:39 -0600

    Follow/Unfollow basic test

--- test/Follows.js
+++ test/Follows.js
@@ -3,12 +3,16 @@ const { ethers, upgrades } = require('hardhat')
 
 describe("Market", function () {
     let owner, bob, alice
-    let FollowManager, FollowManagerDeployer
+    let FollowManager, FollowManagerDeployer, D3jnExternalFollows, D3jnExternalFollowsDeployer
 
 
 
     beforeEach(async function () {
         [owner, bob, alice] = await ethers.getSigners();
+
+        D3jnExternalFollowsDeployer = await ethers.getContractFactory('D3jnExternalFollows')
+
+        D3jnExternalFollows = await D3jnExternalFollowsDeployer.deploy()
 
         FollowManagerDeployer = await ethers.getContractFactory('FollowManager')
 
@@ -20,7 +24,7 @@ describe("Market", function () {
 
         let packed = ethers.utils.solidityPack(
             [ "string", "address", "address" ],
-            [ "User Approves party to follow and unfollow on their behalf", bob.address, owner.address ]
+            [ "User Approves party to follow and unfollow on their behalf", owner.address, bob.address ]
         );
 
         let hashed = ethers.utils.solidityKeccak256(['bytes'],[packed])
@@ -29,7 +33,7 @@ describe("Market", function () {
 
         let splitSig = ethers.utils.splitSignature(sig)
 
-        let result = await FollowManager.VerifyFollowApproval(owner.address, bob.address, splitSig.v, splitSig.r, splitSig.s);
+        let result = await D3jnExternalFollows.VerifyFollowApproval(owner.address, bob.address, splitSig.v, splitSig.r, splitSig.s);
 
         expect(result.toString()).to.equal("true")
 
@@ -44,13 +48,13 @@ describe("Market", function () {
 
         splitSig = ethers.utils.splitSignature(sig)
 
-        result = await FollowManager.VerifyFollowApproval(owner.address, bob.address, splitSig.v, splitSig.r, splitSig.s);
+        result = await D3jnExternalFollows.VerifyFollowApproval(owner.address, bob.address, splitSig.v, splitSig.r, splitSig.s);
 
         expect(result.toString()).to.equal("false")
 
         packed = ethers.utils.solidityPack(
             [ "string", "address", "address" ],
-            [ "User Approves party to follow and unfollow on their behalf", bob.address, owner.address ]
+            [ "User Approves party to follow and unfollow on their behalf", owner.address, bob.address ]
         );
 
         hashed = ethers.utils.solidityKeccak256(['bytes'],[packed])
@@ -59,13 +63,13 @@ describe("Market", function () {
 
         splitSig = ethers.utils.splitSignature(sig)
 
-        result = await FollowManager.VerifyFollowApproval(owner.address, bob.address, splitSig.v, splitSig.r, splitSig.s);
+        result = await D3jnExternalFollows.VerifyFollowApproval(owner.address, bob.address, splitSig.v, splitSig.r, splitSig.s);
 
         expect(result.toString()).to.equal("false")
 
         packed = ethers.utils.solidityPack(
             [ "string", "address", "address" ],
-            [ "User Approves party to follow and unfollow on their behalf", alice.address, owner.address ]
+            [ "User Approves party to follow and unfollow on their behalf", owner.address, alice.address ]
         );
 
         hashed = ethers.utils.solidityKeccak256(['bytes'],[packed])
@@ -74,13 +78,13 @@ describe("Market", function () {
 
         splitSig = ethers.utils.splitSignature(sig)
 
-        result = await FollowManager.VerifyFollowApproval(owner.address, bob.address, splitSig.v, splitSig.r, splitSig.s);
+        result = await D3jnExternalFollows.VerifyFollowApproval(owner.address, bob.address, splitSig.v, splitSig.r, splitSig.s);
 
         expect(result.toString()).to.equal("false")
 
         packed = ethers.utils.solidityPack(
             [ "string", "address", "address" ],
-            [ "User Approves party to follow and unfollow on their behalf", bob.address, alice.address ]
+            [ "User Approves party to follow and unfollow on their behalf", alice.address, bob.address ]
         );
 
         hashed = ethers.utils.solidityKeccak256(['bytes'],[packed])
@@ -89,9 +93,21 @@ describe("Market", function () {
 
         splitSig = ethers.utils.splitSignature(sig)
 
-        result = await FollowManager.VerifyFollowApproval(owner.address, bob.address, splitSig.v, splitSig.r, splitSig.s);
+        result = await D3jnExternalFollows.VerifyFollowApproval(owner.address, bob.address, splitSig.v, splitSig.r, splitSig.s);
 
         expect(result.toString()).to.equal("false")
+
+        addy = await D3jnExternalFollows.VerifyMessage(hashed, splitSig.v, splitSig.r, splitSig.s)
+
+        expect(addy.toString()).to.not.equal(bob.address.toString())
+
+    })
+
+    it('Follow/Unfollow', async function () {
+
+        await FollowManager.follow(bob.address);
+
+        await FollowManager.unfollow(bob.address, 1);
     })
 
 });
