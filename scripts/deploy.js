be49145e5ed6179732e634f7e907f7bddf34505e
DakBuk <brucdarc@gmail.com>
2022-08-26 21:13:58 -0600

    add marketplace deploy to deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,5 @@
+const {ethers} = require("hardhat");
+
 async function main() {
     const [deployer] = await ethers.getSigners();
 
@@ -5,10 +7,15 @@ async function main() {
 
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
-    const factory = await ethers.getContractFactory("NftFactory");
-    const result = await factory.deploy();
+    const NftFactoryDeployer = await ethers.getContractFactory("NftFactory");
+    const NftFactory = await NftFactoryDeployer.deploy();
 
-    console.log("Contract address:", result.address);
+    const MarketPlaceDeployer = await ethers.getContractFactory('SimpleMarketPlace')
+
+    const MarketPlace = await MarketPlaceDeployer.deploy(ethers.utils.parseEther("1.05"), "0x0000000000000000000000000000000000000000")
+
+    console.log("NftFactory address:", NftFactory.address);
+    console.log("MarketPlace address:", MarketPlace.address);
 }
 
 main()
